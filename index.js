e7e2d5fbcb7458331f9c49a9c1182f130298c900
Faezkhan20 <138295726+Faezkhan20@users.noreply.github.com>
2023-12-08 21:12:30 +0530

    update

---
+++ index.js
@@ -0,0 +1,27 @@
+import  express  from "express";
+import router from "./Routes/index.js"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import cors from 'cors'
+
+
+
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
+
+app.use("/hm/api/v1",router)
+
+// app.get("/",function (req,res){
+//     res.send("welcome to youtube beackend server")
+// })
+
+
+
+app.listen(8000,()=>console.log("app is running on server"))
+mongoose.connect(process.env.MONGOURL).then(() => console.log("database connect"))
